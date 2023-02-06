e3f99b3620a681c804f116ee9c80c98a888ee5c4
konssstantin67 <90912446+konssstantin67@users.noreply.github.com>
2023-02-06 13:57:55 +0500

    wdw

--- authors/models.py
+++ authors/models.py
@@ -7,3 +7,6 @@ class Author(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.first_name
